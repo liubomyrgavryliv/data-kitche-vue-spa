27fce8f83b6295b5c24ac15109976417b3beed49
liubomyrgavryliv <liubomyr.gavryliv@gmail.com>
2020-08-25 17:22:37 +0300

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-content transition="slide-x-transition">
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+export default {
+  name: "App"
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
